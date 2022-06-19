09a1ca6640c0ef7ac7bf82dde13fa5a0e4012795
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-19 12:14:29 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function createEmployeeRecord(data) {
+    return newObject ({
+        firstName : data[0], 
+        familyName : data[1], 
+        title : data[2], 
+        payPerHour : data[3], 
+        timeInEvents : [],
+        timeOutEvents : []
+    })
+}
+
+function createEmployeeRecords(){
+    
+}
